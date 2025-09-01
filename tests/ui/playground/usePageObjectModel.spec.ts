72b47fca45ba4aacb8a395c22fcb0feaee77c1ff
Patrick So <patrickso@Mac.lan>
2025-09-01 13:20:37 +1000

    added class attribute test

--- tests/ui/playground/usePageObjectModel.spec.ts
+++ tests/ui/playground/usePageObjectModel.spec.ts
@@ -1,26 +1,34 @@
 import { test, expect } from '@playwright/test';
-import { NavigationPage } from './page-objects/navigationPage';
-import { AjaxPage } from './page-objects/ajaxPage';
 import { PageManager } from './page-objects/pageManager';
 
 test('AJAX button triggers content (UITestingPlayground)', async ({ page }) => {
-    const nav = new NavigationPage(page);
-    const ajax = new AjaxPage(page);
+    const pm = new PageManager(page);
 
-    await nav.gotoAjax();                 // centralized navigation
-    await ajax.triggerAjaxRequest();      // page-specific action
-    await ajax.waitForAjaxResponseOk();   // network-level confirmation
-    await ajax.expectDataLoadedVisible(); // UI confirmation
+    await pm.navigateTo().ajaxPage();
+    await pm.onAjax.triggerAjaxRequest();    // centralized navigation
+    await pm.onAjax.waitForAjaxResponseOk();
+    await pm.onAjax.expectDataLoadedVisible();    // page-specific action
 });
 
 test('Dynamic Table: Chrome CPU equals bottom label value', async ({ page }) => {
     const pm = new PageManager(page);
 
-    await pm.navigateTo().dynamicTable(); // using PageManager for navigation
+    await pm.navigateTo().dynamicTablePage(); // using PageManager for navigation
 
-    const tableValue = await pm.onDynamicTable.cellText('Chrome', 'CPU');
+    const tableValue = await pm.onDynamicTable.captureValueofCpu('Chrome');
     const summaryValue = await pm.onDynamicTable.chromeCpuSummaryValue();
 
     console.log(`Asserting: TableValue='${tableValue}' vs SummaryValue='${summaryValue}'`);
     expect(tableValue).toBe(summaryValue);
+});
+
+test('Class Attribute button is visible and clickable', async ({ page }) => {
+    const pm = new PageManager(page);
+
+    await pm.navigateTo().classAttributePage();
+    const message = await pm.onClassAttribute.clickButtonAndAcceptAlert();
+    // Assert the alert text is correct
+    expect(message).toContain('Primary button pressed');
+    await pm.onClassAttribute.expectNoDialogWithin(500);
+    
 });
